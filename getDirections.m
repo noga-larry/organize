6e3e56b2efb3838f0ef3f8abcf25ae9a48037a5b
Noga Larry <noga.larry@mail.huji.ac.il>
2018-11-28 11:39:28 +0200

    Add file acel reading function

--- getDirections.m
+++
@@ -1,8 +0,0 @@
-function [directions,match_d] = getDirections (data)
-
-expression = '(?<=d)[0-9]*'; 
-[match_d,~] = regexp({data.trials.name},expression,'match','split','forceCellOutput','once');
-directions = uniqueRowsCA(match_d');
-directions = natsortfiles(directions');
-directions(cellfun('isempty',directions)) = [];
-y=9
---
+++ get_excel_info.m
@@ -0,0 +1,20 @@
+function get_excel_info(excel_file_adress, dir_to)
+
+% This function reads the sessios exel database and creates a matlab 
+% structure with the cells info using the first row as fields name. It then
+% saves the matla structure. 
+%Inputs:     excel_file_adress   location of the file
+%            dir_to              where to save the data strcture
+
+[~,txt,raw] = xlsread(excel_file_adress);
+
+for ii=1:size(raw,2)
+    varName = txt{1,ii};
+    %get feild name
+    varData = raw (2:end,ii); 
+    [task_info(1:length(varData)).(varName)]=deal(varData{:});
+end
+  
+save(dir_to,'task_info')
+end
+
