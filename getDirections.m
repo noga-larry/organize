193ad1b2d094e18ae86129135e623a46d2d1302f
Noga Larry <noga.larry@mail.huji.ac.il>
2019-03-31 17:15:23 +0300

    Change function to output arrays instead of cells

--- getDirections.m
+++ getDirections.m
@@ -12,6 +12,6 @@ function [directions,match_d] = getDirections (data)
 expression = '(?<=d)[0-9]*'; 
 [match_d,~] = regexp({data.trials.name},expression,'match','split','forceCellOutput');
 match_d = reshape([match_d{:}],length(match_d{1}),length(match_d));
-directions = uniqueRowsCA({match_d{:}}');
-directions = natsortfiles(directions');
-directions(cellfun('isempty',directions)) = [];
+match_d =  cellfun(@str2double,match_d);
+directions = unique(match_d);
+directions = sort(directions);
--- getProbabilities.m
+++ getProbabilities.m
@@ -13,6 +13,7 @@ function [probabilities,match_p] = getProbabilities (data)
 expression = '(?<=P)[0-9]*|(?<=p)[0-9]*'; 
 [match_p,~] = regexp({data.trials.name},expression,'match','split','forceCellOutput');
 match_p = reshape([match_p{:}],length(match_p{1}),length(match_p));
-probabilities = uniqueRowsCA({match_p{:}}');
-probabilities = natsortfiles(probabilities');
-probabilities(cellfun('isempty',probabilities)) = [];
+match_p =  cellfun(@str2double,match_p);
+probabilities = unique(match_p);
+probabilities = sort(probabilities);
+
