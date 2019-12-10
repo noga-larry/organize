4d067330261e0afd789205be7e5be4e91f81a48b
Noga Larry <noga.larry@mail.huji.ac.il>
2019-12-10 11:31:01 +0200

    Initial commir for getPreviousCompleted

---
+++ getPreviousCompleted.m
@@ -0,0 +1,13 @@
+function data = getPreviousCompleted(data,MaestroPath)
+% This function adds a fields to the strcture data, 'previous_completed' that
+% is 1 if the previous trial was completed and 0 else.
+
+
+threshold = 1000;
+
+for t = 1:length(data.trials)
+    extended = importdata (  [MaestroPath '\'  data.info.session ...
+        '\extend_trial\' data.trials(t).maestro_name '.mat']);
+    data.trials(t).previous_completed = any(extended.rwd(1:extended.trial_begin_ms) > threshold);
+    
+end
