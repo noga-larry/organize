a88b585bcf4c1331193ce52669ab8864888064c1
nogalarry <noga.larry@mail.huji.ac.il>
2020-04-28 15:48:19 +0300

    task folder inside of fingPathsToCells function

--- findPathsToCells.m
+++ findPathsToCells.m
@@ -1,5 +1,6 @@
 function paths = findPathsToCells (supPath,task_info,lines)
 
+supPath = [supPath '\' task_info(lines(1)).task '\'];
 names =  {task_info(lines).save_name};
 paths = cellfun(@(c)[supPath '\' c '.mat'],names,'uni',false);
 
