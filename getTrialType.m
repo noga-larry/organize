623126a23755bb04368dcb6543f516bae3247164
nogalarry <noga.larry@mail.huji.ac.il>
2020-10-18 07:53:48 +0300

    trialType change and chage in listSessions to separate by screen rotation

--- getTrialType.m
+++ getTrialType.m
@@ -1,12 +1,11 @@
 function task = getTrialType(data_raw)
-if ~isempty(regexp(data_raw.trialname,'vs'))
-    task = 'choice';
-elseif ~isempty(regexp(data_raw.trialname,'v[1-9]*s'))
-    task = 'speed';
-elseif ~isempty(regexp(data_raw.trialname,'v'))
-    task = 'pursuit';
-else
-    task = 'saccade';
-    
-end
+    if ~isempty(regexp(data_raw.trialname,'vs'))
+        task = 'choice';
+    elseif ~isempty(regexp(data_raw.trialname,'v[1-9]*s'))
+        task = 'pursuit_8_dir_75and25';
+    elseif ~isempty(regexp(data_raw.trialname,'v'))
+        task = 'pursuit_8_dir_75and25';
+    else
+        task = 'saccade_8_dir_75and25';
+    end
 end
--- listSessionsFromTrials.m
+++ listSessionsFromTrials.m
@@ -11,12 +11,14 @@ for s = 1:length(prefix)
     end
     data_raw = readcxdata(  [dirFrom '\'  prefix{s} 'a' sprintf('.%04d',1)]);
     taskBefore = getTrialType(data_raw);
+    thetaBefore = double(data_raw.key.iPosTheta/1000);
     fb = 1;
     for ii=1:numTrials
         data_raw = readcxdata(  [dirFrom '\'  prefix{s} 'a' sprintf('.%04d',ii)]);
         taskCurrent = getTrialType(data_raw);
-        if ~strcmp(taskBefore,taskCurrent) || (ii==numTrials)
-            if ~strcmp(taskBefore,taskCurrent)
+        thetaCurrent = double(data_raw.key.iPosTheta/1000);
+        if ~strcmp(taskBefore,taskCurrent) || ~(thetaCurrent==thetaBefore) || (ii==numTrials)
+            if ~strcmp(taskBefore,taskCurrent) || ~(thetaCurrent==thetaBefore)
                 fe = ii-1;
             elseif (ii==numTrials)
                 fe = ii;
@@ -26,8 +28,10 @@ for s = 1:length(prefix)
             task_info(counter).file_begin = fb;
             task_info(counter).file_end = fe;
             task_info(counter).trial_type = 'a'; 
+            task_info(counter).screen_rotation = thetaBefore; 
             counter = counter+1;
             taskBefore = taskCurrent;
+            thetaBefore = thetaCurrent;
             fb = fe+1;
         end
     end
