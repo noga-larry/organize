52adc65605a7ea3934210ce645a473dcc8fc195b
nogalarry <noga.larry@mail.huji.ac.il>
2021-07-15 14:46:40 +0300

    getLicking: save the beginning of extended in the trial

--- getLicking.m
+++ getLicking.m
@@ -3,9 +3,9 @@ threshold = 5000;
 flagCross = false;
 for t=1:length(data.trials)
     extended = importdata (  [maestroPath '\' data.info.monkey '\' data.info.session ...
-        '\extend_trial\' data.trials(t).maestro_name '.mat']);
-    
+        '\extend_trial\' data.trials(t).maestro_name '.mat']);   
     data.trials(t).lick = extended.lick;
+    data.trials(t).extended_trial_begin = extended.trial_begin_ms;
     if any(data.trials(t).lick >threshold)
         flagCross = true;
     end
