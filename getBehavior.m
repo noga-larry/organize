2346260f51ed6dd7ef4809bf733a27f2362328c0
nogalarry <noga.larry@mail.huji.ac.il>
2022-01-23 14:10:27 +0200

    Add shadow files for behavior

--- getBehavior.m
+++ getBehavior.m
@@ -1,23 +1,16 @@
-function data = getBehavior (data,maestroPath);
+function data = getBehavior (data,supPath)
 
-CALIBRATE_VEL = 10.8826;
-CALIBRATE_POS = 40;
+path = [supPath '\' data.info.task '\behavior\' ...
+    data.info.behavior_shadow_name];
+behavior = importdata(path);
 
+for t = 1:length(data.trials)
+    assert(strcmp(data.trials(t).maestro_name,...
+        behavior.trials(t).maestro_name))
+    data.trials(t).hPos = behavior.trials(t).hPos;
+    data.trials(t).vPos = behavior.trials(t).vPos;
+    data.trials(t).hVel = behavior.trials(t).hVel;
+    data.trials(t).vVel = behavior.trials(t).vVel;
+end
 
-
-
-for t=1:length(data.trials)
-data_raw = readcxdata (  [maestroPath '\' data.info.monkey '\'  data.info.session ...
-   '\' data.trials(t).maestro_name]);
-
-% get behavior
-%  1: horizonal position
-%  2: vertical position
-%  3: horizonal velocity
-%  4: vertical velocity
-
-data.trials(t).hPos = data_raw.data(1,:)/CALIBRATE_POS;
-data.trials(t).vPos = data_raw.data(2,:)/CALIBRATE_POS;
-data.trials(t).hVel = data_raw.data(3,:)/CALIBRATE_VEL;
-data.trials(t).vVel = data_raw.data(4,:)/CALIBRATE_VEL;
 end
---
+++ getBehaviorShadowFile.m
@@ -0,0 +1,1 @@
+function behaviorData = getBehaviorShadowFile(data)
