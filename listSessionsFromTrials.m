93bb3c5a65dcb828a12e88520e429156c562d4d0
nogalarry <noga.larry@mail.huji.ac.il>
2022-02-09 17:54:13 +0200

    listSessionFromTrials: get trial numbers from the files

--- listSessionsFromTrials.m
+++ listSessionsFromTrials.m
@@ -1,28 +1,44 @@
 function task_info = listSessionsFromTrials(supDirectory,prefix)
- 
+
+
+monkeyList =...
+    {'albert',
+    'chips',
+    'bissli',
+    'yoda',
+    'reggie',
+    'pluto',
+    'imonkey',
+    'xtra',
+    'golda'};
+monkeyName =  containers.Map(cellfun(@(x) x(1:2), monkeyList, 'un', 0),monkeyList);
+
+
+
 counter = 1;
 for s = 1:length(prefix)
-    dirFrom = [supDirectory '\' prefix{s} '\'];
+    curr_prefix = prefix{s};
+    dirFrom = [supDirectory '\' monkeyName(curr_prefix(1:2)) '\' curr_prefix '\'];
     files = dir(dirFrom); files = files (3:end);
     
-    numTrials = cellfun(@str2double,regexp({files.name},'(?<=a.)[0-9]*','match'),'UniformOutput',false);
-    numTrials = max([numTrials{:}]);
-    if numTrials==0
+    numTrials = cellfun(@str2double,regexp({files.name},'(?<=(a\.))[0-9]*','match'),'UniformOutput',false);
+    numTrials = sort([numTrials{:}]);
+    if isempty(numTrials)
         continue
     end
-    data_raw = readcxdata(  [dirFrom '\'  prefix{s} 'a' sprintf('.%04d',1)]);
+    data_raw = readcxdata(  [dirFrom '\'  prefix{s} 'a' sprintf('.%04d',numTrials(1))]);
     taskBefore = getTrialType(data_raw);
     thetaBefore = double(data_raw.key.iPosTheta/1000);
     fb = 1;
-    for ii=1:numTrials
-        data_raw = readcxdata(  [dirFrom '\'  prefix{s} 'a' sprintf('.%04d',ii)]);
+    for ii=1:length(numTrials)
+        data_raw = readcxdata(  [dirFrom '\'  prefix{s} 'a' sprintf('.%04d',numTrials(ii))]);
         taskCurrent = getTrialType(data_raw);
         thetaCurrent = double(data_raw.key.iPosTheta/1000);
-        if ~strcmp(taskBefore,taskCurrent) || ~(thetaCurrent==thetaBefore) || (ii==numTrials)
+        if ~strcmp(taskBefore,taskCurrent) || ~(thetaCurrent==thetaBefore) || (ii==length(numTrials))
             if ~strcmp(taskBefore,taskCurrent) || ~(thetaCurrent==thetaBefore)
-                fe = ii-1;
-            elseif (ii==numTrials)
-                fe = ii;
+                fe = numTrials(ii)-1;
+            elseif (numTrials(ii)==length(numTrials))
+                fe = numTrials(ii);
             end
             task_info(counter).session = prefix{s};
             task_info(counter).task = taskBefore;
@@ -30,6 +46,7 @@ for s = 1:length(prefix)
             task_info(counter).file_end = fe;
             task_info(counter).trial_type = 'a'; 
             task_info(counter).screen_rotation = thetaBefore; 
+            task_info(counter).num_trials = fe-fb; 
             counter = counter+1;
             taskBefore = taskCurrent;
             thetaBefore = thetaCurrent;
